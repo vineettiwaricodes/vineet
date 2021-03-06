8ebebe5ed55ff64c6666427f1fd3b8964e7ce7a5
7678549340 <35331937+7678549340@users.noreply.github.com>
2021-03-06 13:59:30 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+var arr=[
+  {
+    student_data:{
+      name:'A',
+      id: 123,
+      marks:[{'hi':11,'mt':22,'en':20}]
+    }}
+  ,
+  {
+    student_data:{
+      name:'B',
+      id: 124,
+      marks:[{'hi':12,'mt':20,'en':10}]
+    }
+  },
+  {
+    student_data:{
+      name:'C',
+      id: 125,
+      marks:[{'hi':12,'mt':22,'en':23}]
+    }
+  }
+]
+console.log(arr.student_data);
+
+// var sort_by = function(id, name, total_marks){
+ var key = initial ?
+       function(x)
+             {
+               return initial(x[field_name]);
+             } :
+       function(x) 
+             {
+               return x[field_name];
+             };
+
+
+// var newobj = info.sort(sort_by(id, name, totalmarks));
+
+var ab=console.log(arr[0]);
